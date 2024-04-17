f14e4bb822ab989cc3d504a4bf2e14a90339f6fd
Soulgrimm <Dante94_94@bk.ru>
2024-04-17 06:09:51 +0300

    Second

--- materials/models.py
+++ materials/models.py
@@ -3,25 +3,10 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Lesson(models.Model):
-    title = models.CharField(max_length=100, verbose_name='название')
-    picture = models.ImageField(upload_to='materials/', verbose_name='картинка', **NULLABLE)
-    description = models.TextField(max_length=200, verbose_name='описание')
-    url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.title}'
-
-    class Meta:
-        verbose_name = "Урок"
-        verbose_name_plural = "Уроки"
-
-
 class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name='название')
     picture = models.ImageField(upload_to='materials/', verbose_name='картинка', **NULLABLE)
     description = models.TextField(max_length=200, verbose_name='описание')
-    lessons = models.ManyToManyField(Lesson, verbose_name='Уроки для курса')
 
     def __str__(self):
         return f'{self.title}'
@@ -29,3 +14,18 @@ class Course(models.Model):
     class Meta:
         verbose_name = "Курс"
         verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+    picture = models.ImageField(upload_to='materials/', verbose_name='картинка', **NULLABLE)
+    description = models.TextField(max_length=200, verbose_name='описание')
+    url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
